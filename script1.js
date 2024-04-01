777f5f38cd2aa201f1b036f7ea346c40aeaf1886
rajasekar47 <149884742+rajasekar47@users.noreply.github.com>
2024-04-01 10:20:26 +0530

    Update script1.js

--- script1.js
+++ script1.js
@@ -1,20 +1,30 @@
-//Question 1:
-//for the below JSON iterate over
-//for in loop
-var mydetails = {
-    "Name":"Rajasekar",
-    "Age":27,
-    "Degree":"B.Arch",
-    "Address":"Gingee,Villupuram",
-    "Number":"+916381388939",
-    "profiles": [
-        {
-          "website": "https://www.linkedin.com/in/rajasekar-k-7458692b2",
-          "github":"https://github.com/rajasekar47"
-        }
-      ]
+//1.  JSON iterate for loops (For, For in, for of, forEach)
+
+var car = [{
+    "Name": "i20",
+  "company": "Hyundai",
+  "variants": ["sports", "Asta", "Magna","Era"],
+  "Fuel Type": ["Diesel","Petrol"],
+  "Body type": "Hatchback",
+  "Ratings": ["5","4","3","2","1"],
+  "keyFeatures": ["Airbag","Alloy Wheel", "Foglights", "power steering", "AC"]      
+  }];
+  console.log(car);
+
+  // For loop
+  for ( var i=0;i<car.length;i++){
+    console.log(car[i].keyFeatures);
     };
-    
-    for (var i in mydetails){
-    console.log(i,mydetails[i]);
+  
+  // For in loop
+   for ( var a in car){
+  console.log(car[a].variants);
+  }
+  
+// For of 
+for ( var val of car){
+    console.log(val.keyFeatures);
     }
+ 
+// for each 
+      car.forEach(function(b) { console.log(b.Ratings); });
